72974ba2b402f0ac5f8cd462aa89bc4140f6daa5
Luxurno Marcin Szostak <luxurno@luxurno.com>
2021-05-15 16:22:03 +0200

    Adding Chapter 5

---
+++ chapter-5-2-an-example-program-using-structs/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let rect1 = Reactangle {
+        width: 30,
+        height: 50,
+    };
+
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     area(&rect1)
+    // )
+    println!("rect1 is {:?}", rect1);
+}
+
+fn area(rectangle: &Reactangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+#[derive(Debug)]
+struct Reactangle {
+    width: u32,
+    height: u32,
+}
